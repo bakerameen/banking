784141f0a1beca5ea65419fa61bfc26d16acd8ca
Baker <bakerameen@gmail.com>
2024-05-25 12:35:49 +0300

    Implement Header

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,16 @@
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = { firstName: 'Adrian' }
   return (
     <section className='home'>
       <div className='home-content'>
         <header>
-          Welcom , Adrian
+          <HeaderBox type="greeting" title="welcom" user={loggedIn.firstName || 'Guest'} subtext='Access and manage your account and transaction' />
+
+          <TotalBalanceBox></TotalBalanceBox>
         </header>
       </div>
     </section>
